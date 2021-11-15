603263b90e7ea8bd1b9db84aced8ba214839bccc
임동현 <79711744+ldh3907@users.noreply.github.com>
2021-11-15 20:15:04 +0900

    update : LoginPage
    update : LoginPage

--- src/Pages/Login/LoginPage.tsx
+++ src/Pages/Login/LoginPage.tsx
@@ -1,11 +1,43 @@
 import useLogin from "src/Hooks/Login/useLogin";
+import styled from "styled-components";
+
+const LoginContainer = styled.div`
+  width: 100%;
+  height: 700px;
+  display: flex;
+`;
+
+const LoginLoader = styled.div`
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  border: 8px solid var(--main-color);
+  border-radius: 50%;
+  border-top-color: #fff;
+  animation: spin 1s ease-in-out infinite;
+  -webkit-animation: spin 1s ease-in-out infinite;
+  margin: auto auto;
+
+  @keyframes spin {
+    to {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+  @-webkit-keyframes spin {
+    to {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+`;
 
 const LoginPage: React.FC = () => {
   const { onSubmit } = useLogin();
 
   return (
     <>
-      <div>Loading...</div>
+      <LoginContainer>
+        <LoginLoader />
+      </LoginContainer>
     </>
   );
 };
